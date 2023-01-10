3d78d88b96a5169dbed431bcff8f59b70f71df9c
HyeonSeuk01~ <seo5719@naver.com>
2023-01-10 15:44:29 +0900

    update

---
+++ Day7.ex/tr.py
@@ -0,0 +1,46 @@
+# Q1.
+# a = int(input('정수를 출력하세요'))
+# print(a)
+
+# Q2.
+# a = input('단어를 구분해서 출력하세요')
+# print(a)
+
+# Q3.
+# T = int(input())
+
+# for t in range(1, T+1):
+#     print(t)
+    
+#     pass
+
+# Q4.
+# numbers = list(map(int,input('2개 이상의 정수를 출력하세요').split()))
+
+# print(numbers)
+
+# Q5.
+# a, b = list(map(int,input('2개의 정수를 출력하세요').split()))
+
+# print(a, b)
+
+# Q6.
+# a = input('단어를 구분해서 출력하세요').split()
+# print(a)
+
+# Q7.
+# T = int(input())
+
+# for t in range(1, T+1):
+#     numbers = list(map(int,input().split()))
+#     print(numbers)
+
+# Q8.
+# T = int(input())
+
+# for t in range(1, T+1):
+#     numbers = list(map(int,input().split()))
+#     print(numbers)
+    
+
+    
---
+++ Day7.pr/pr.py
@@ -0,0 +1,31 @@
+# 몫과 나머지 출력하기
+# T = int(input())
+# for t in range(1, T+1):
+#     a, b = list(map(int, input().split()))
+#     s = a // b
+#     r = a % b
+#     print(f"#{t} {s} {r}")
+
+# 평균값 구하기
+# T = int(input())
+# for t in range(1, T+1):
+#     a = list(map(int, input().split()))
+#     average = round(sum(a)/10)
+#     print(f"#{t} {average}")
+
+# 아주 간단한 계산기
+# a, b = list(map(int, input().split()))
+# calculator = [a+b, a-b, a*b, a//b]
+# for i in calculator:
+#     print(i)
+
+# 스탬프 찍기
+# a = int(input())
+# for i in range(a):
+#     print('#', end='')
+
+# 최대수 구하기
+# T = int(input())
+# for t in range(1, T+1):
+#     a = list(map(int,input().split()))
+#     print(f'#{t} {max(a)}')
