a7a27e211053ebd564027c186f4721a31fe6828c
HyeonSeuk01~ <seo5719@naver.com>
2023-01-12 00:42:25 +0900

    update

---
+++ Day8.pr/p.py
@@ -0,0 +1,35 @@
+# 신문 헤드라인
+# print(input().upper())
+
+# N줄덧셈
+# n = int(input())
+# a = range(1, n+1)
+# print(sum(a))
+
+# 1대1 가위바위보
+# a, b = map(int, input().split())
+# if a > b:
+#     print('A')
+# elif b > a:
+#     print('B')
+
+# 대각선 출력하기
+# print('#++++')
+# print('+#+++')
+# print('++#++')
+# print('++++#')
+
+# 자릿수 더하기
+# a = int(input())
+# n = 0
+
+# while a > 0:
+#     n += a%10
+#     a //= 10    
+# print(n)
+
+# 더블더블
+# a = int(input())
+# for i in range(0, a+1):
+#     print(2**i, end=' ')
+    
