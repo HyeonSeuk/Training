281958cbc231e4c98dce4e95b59cba5bc701d26c
HyeonSeuk01~ <seo5719@naver.com>
2023-01-16 11:45:22 +0900

    update

---
+++ Day13.ex/tr.py
@@ -0,0 +1,41 @@
+# 1000 A+B
+# a, b = list(map(int, input().split()))
+# print(a+b)
+
+
+# 2558 A+B-2
+# a = int(input())
+# b = int(input())
+# print(a+b)
+
+
+# 10950 A+B-3
+# T = int(input())
+
+# for t in range(1, T+1):
+#     a, b = list(map(int, input().split()))
+#     print(a+b)
+
+
+# 10953 A+B-6
+# T = int(input())
+
+# for t in range(T):
+#     a, b = map(int, input().split(','))
+#     print(a+b)
+
+
+# 11021 A+B-7
+# T = int(input())
+
+# for t in range(1, T+1):
+#     a = list(map(int, input().split()))
+#     print(f'Case #{t}: {sum(a)}')
+
+
+# 11022 A+B-8
+# T = int(input())
+
+# for t in range(1, T+1):
+#     a = list(map(int, input().split()))
+#     print(f'Case #{t}: {a[0]} + {a[1]} = {sum(a)}')
