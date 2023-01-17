1230a6e302fd8db2677666ac62024004d6327dc2
HyeonSeuk01~ <seo5719@naver.com>
2023-01-17 14:09:54 +0900

    update

---
+++ Day14.ex/tr.py
@@ -0,0 +1,30 @@
+# 10818 최소, 최대
+# n = int(input())
+# a = list(map(int, input().split()))
+# print(min(a), max(a))
+
+# 11720 숫자의 합
+# total = 0
+# n = int(input())
+# numbers = input()
+# for i in range(n):
+#     total += int(numbers[i])
+# print(total)
+
+# 2750 수 정렬하기
+# n = int(input())
+# list = []
+# for i in range(n):
+#     number = int(input())
+#     list.append(number)
+#     list.sort()
+# for j in range(len(list)):
+#     print(list[j])
+
+# 2562 최댓값
+# list = []
+# for i in range(9):
+#     n = int(input())
+#     list.append(n)
+# print(max(list))
+# print(list.index(max(list))+1)
