8094e73aee1808074225b4cb93a9d71fdd843d56
HyeonSeuk01~ <seo5719@naver.com>
2023-01-28 00:12:59 +0900

    update

--- Day24.py/pr.py
+++ Day24.py/pr.py
@@ -54,5 +54,16 @@
 # print(*sorted(lst_set, key=len),sep='\n')
 
 # 절댓값 힙
-
+# import heapq
+# import sys
+# heap = []
+# for i in range(int(sys.stdin.readline())):
+#     a = int(sys.stdin.readline())
+#     if a != 0:
+#         heapq.heappush(heap, (abs(a), a))
+#     else:
+#         if heap:
+#             print(heapq.heappop(heap)[1])
+#         else:
+#             print(0)
     
